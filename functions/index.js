ca5d92f79c7113c32bef982749bc950f3eee24fd
Michael Kyzyma <mkyzyma@gmail.com>
2020-04-16 11:30:43 +0500

    Added dependencies

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,4 @@
 const functions = require('firebase-functions');
-// We have to import the built version of the server middleware.
 // eslint-disable-next-line import/no-unresolved
 const { sapper } = require('./__sapper__/build/server/server');
 
